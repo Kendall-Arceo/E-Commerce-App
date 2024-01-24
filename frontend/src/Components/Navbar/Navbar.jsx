f87a1244a493adfe5d862c487646d393a449c565
unknown <arceokendall@gmail.com>
2024-01-24 15:56:15 -0600

    adding links and reactrouter items.

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -17,18 +17,18 @@ export const Navbar = () => {
 
         <ul className = "nav-menu">
           <li onClick={()=>{setMenu("shop")}}>
-            <Link to ='/'>Shop</Link> {menu === "shop"?<hr/>:<></>} {/* turnary operator */}
+            <Link style={{ textDecoration: 'none'}} to ='/'>Shop</Link> {menu === "shop"?<hr/>:<></>} {/* turnary operator */}
           </li>
 
           <li onClick={()=>{setMenu("mens")}}>
-            <Link to ='/mens'>Men</Link> {menu === "mens"?<hr/>:<></>}
+            <Link style={{ textDecoration: 'none'}} to ='/mens'>Men</Link> {menu === "mens"?<hr/>:<></>}
           </li>
 
           <li onClick={()=>{setMenu("womens")}}>
-            <Link to ='/womens'>Women</Link>{menu === "womens"?<hr/>:<></>}
+            <Link style={{ textDecoration: 'none'}} to ='/womens'>Women</Link>{menu === "womens"?<hr/>:<></>}
           </li>
           <li onClick={()=>{setMenu("kids")}}>
-            <Link to ='/kids'>Kids</Link>{menu === "kids"?<hr/>:<></>}
+            <Link style={{ textDecoration: 'none'}} to ='/kids'>Kids</Link>{menu === "kids"?<hr/>:<></>}
           </li>
         </ul>
 
